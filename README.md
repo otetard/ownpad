2b324e050fd0862e7e08e34adc1341b44efeaed8
Olivier Tétard <olivier.tetard@miskin.fr>
2016-07-26 17:34:11 +0200

    Disable mimetype registration since it breaks everything (see #1).
    To make everything work properly, you need to manually add the
    required mimetypes at ownCloud level. Everything is explained in
    `README.md`.

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ access to an instance of Etherpad and/or Ethercalc to work properly.
 This application works pretty much like
 [`files_etherpad`](https://github.com/EELV-fr/Owncloud-Ether-Docs) but
 is a complete rewrite.
+
+## Mimetype detection
+
+Unfortunatly, apps can’t declare new mimetypes on the fly. To make
+OwnPad work properly, you need to add two new mimetypes in the
+`mimetypemapping.json` file (at ownCloud level).
+
+To proceed, just copy `/resources/config/mimetypemapping.dist.json` to
+`/config/mimetypemapping.json`, and then add the two following lines
+just after the “_comment” lines.
+
+    "pad": ["application/x-ownpad"],
+    "calc": ["application/x-ownpad"],
+
+If all other mimetypes are not working properly, just run the
+following command:
+
+    sudo -u www-data php occ files:scan --all
--- appinfo/app.php
+++ appinfo/app.php
@@ -14,5 +14,17 @@ OCP\App::registerAdmin('ownpad','settings');
 OCP\Util::addscript('ownpad', 'ownpad');
 OCP\Util::addStyle('ownpad', 'ownpad');
 
-\OC::$server->getMimeTypeDetector()->registerType("pad", "application/x-ownpad");
-\OC::$server->getMimeTypeDetector()->registerType("calc", "application/x-ownpad");
+/*
+ * Disabled since it breaks Mimetype detection for other types…
+ * More information here: https://github.com/otetard/ownpad/issues/3
+ *
+ * To make everything work, you need to create
+ * `/config/mimetypemapping.json` from
+ * `/resources/config/mimetypemapping.dist.json` and then append the
+ * two following lines just after the “_comment” lines.
+ *
+ *     "pad": ["application/x-ownpad"],
+ *     "calc": ["application/x-ownpad"],
+ */
+// \OC::$server->getMimeTypeDetector()->registerType("pad", "application/x-ownpad");
+// \OC::$server->getMimeTypeDetector()->registerType("calc", "application/x-ownpad");
