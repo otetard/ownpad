otetard/ownpad

commit bb3f3199c44d35b21a45d1ae6dd5524853f401cf

\OCP\Util::isValidFileName is deprecated, replace it with verifyPath().

lib/Controller/AjaxController.php
@@ -102,7 +102,10 @@ class AjaxController extends Controller {
             return $this->error($l10n->t('Incorrect padname.'));
         }
 
-        if(!\OCP\Util::isValidFileName($padname)) {
+        try {
+            \OC\Files\View::verifyPath($dir, $padname);
+        }
+        catch(\OCP\Files\InvalidPathException $ex) {
             return $this->error($l10n_files->t("Invalid name, '\\', '/', '<', '>', ':', '\"', '|', '?' and '*' are not allowed."));
         }
 

commit 829d0ed9479d7357092ce90eeccf6f1ff155bbe6

Fix verifyPath() usage.
This bug was introduced in bb3f3199c44d35b21a45d1ae6dd5524853f401cf

lib/Controller/AjaxController.php
@@ -103,7 +103,8 @@ class AjaxController extends Controller {
         }
 
         try {
-            \OC\Files\View::verifyPath($dir, $padname);
+            $view = new \OC\Files\View();
+            $view->verifyPath($dir, $padname);
         }
         catch(\OCP\Files\InvalidPathException $ex) {
             return $this->error($l10n_files->t("Invalid name, '\\', '/', '<', '>', ':', '\"', '|', '?' and '*' are not allowed."));