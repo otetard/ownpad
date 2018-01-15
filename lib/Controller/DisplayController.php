f6edc9ddfe020ed3eb8ecd06da99c221dc5f5a61
Olivier Tétard <olivier.tetard@miskin.fr>
2018-01-15 10:46:57 +0100

    Update copyright header

--- lib/Controller/DisplayController.php
+++ lib/Controller/DisplayController.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * ownCloud - Ownpad
+ * Nextcloud - Ownpad
  *
  * This file is licensed under the Affero General Public License
  * version 3 or later. See the COPYING file.
  *
  * @author Olivier Tétard <olivier.tetard@miskin.fr>
- * @copyright Olivier Tétard <olivier.tetard@miskin.fr>, 2015
+ * @copyright Olivier Tétard <olivier.tetard@miskin.fr>, 2017
  */
 
 namespace OCA\Ownpad\Controller;
--- lib/Migration/MigrateSettings.php
+++ lib/Migration/MigrateSettings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ownCloud - Ownpad
+ * Nextcloud - Ownpad
  *
  * This file is licensed under the Affero General Public License
  * version 3 or later. See the COPYING file.
