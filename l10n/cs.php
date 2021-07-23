f2e40fc322b73b01c7d0de0d8b17335b2883c3b5
p-bo <pavel.borecki@gmail.com>
2021-07-23 12:02:08 +0200

    Add Czech localization

---
+++ l10n/cs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Nextcloud - Ownpad
+ *
+ * This file is licensed under the Affero General Public License
+ * version 3 or later. See the COPYING file.
+ *
+ * @author Olivier Tétard <olivier.tetard@miskin.fr>
+ * @copyright Olivier Tétard <olivier.tetard@miskin.fr>, 2017
+ */
+
+$TRANSLATIONS = array(
+    'Calc' => 'Společně vytvářené tabulky',
+    'Collaborative Documents'=>'Společně vytvářené dokumenty',
+    'Edit' => 'Upravit',
+    'Ethercalc Host' => 'Hostitel s Ethercalc',
+    'Etherpad Host' => 'Hostitel s Etherpad',
+    'Etherpad cookie domain' => 'Doména s Etherpad cookie',
+    'Incorrect pad name' => 'Nesprávný název pro blok',
+    'New calc.calc' => 'Nova_tabulka.calc'
+    'New pad.pad' => 'Novy_blok.pad',
+    'New protected pad' => 'Nový chráněný blok',
+    'Pad' => 'Blok',
+    'public link' => 'Veřejný odkaz',
+    'Protected Pad' => 'Chráněný blok',
+    'Saved' => 'Uloženo',
+    'This is used to link collaborative documents inside Nextcloud.' => 'Toto slouží pro odkazování na společně vytvářené dokumenty v rámci Nextcloud',
+    'Title' => 'Nadpis',
+    'Unable to communicate with Etherpad API.' => 'Nedaří se komunikovat s aplikačním program. rozhraním (API) Etherpad serveru',
+    'Use Etherpad API (*experimental*)' => 'Použít aplikační program. rozhraní (API) Etherpad serveru (*experimentální*)',
+    'You need to enable Etherpad API if you want to create “protected” pads, that will only be accessible through Nextcloud. To make this work, you need to host your Etherpad instance in a sub of sibbling domain of the one that is used by Nextcloud (due to cookie isolation).' => 'Pokud chcete vytvářet „chráněné“ bloky, je třeba zapnout aplikační program. rozhraní (API) na Etherpad serveru, které bude přístupné pouze prostřednictvím Nextcloud. Aby to fungovalo, je třeba hostovat instanci Etherpad na podřízené nebo sesterské doméně té, použité pro Nextcloud (z důvodu izolace cookie).',
+    'Your Etherpad/Ethercalc document could not be opened: the URL specified in your file does not match the allowed server.' => 'Etherpad/Ethercalc dokument se nepodařilo otevřít: URL adresa, uvedená v souboru se neshoduje s povoleným serverem.',
+);
